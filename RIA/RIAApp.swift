39831ce09cdd307836e54e49d0090539e345747f
이대현 <ej4658@naver.com>
2024-07-28 15:04:15 +0900

    프로젝트 초기 세팅

---
+++ RIA/RIAApp.swift
@@ -0,0 +1,17 @@
+//
+//  RIAApp.swift
+//  RIA
+//
+//  Created by 이대현 on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct RIAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
